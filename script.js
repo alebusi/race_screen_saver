3713c24438bd922c54499c3ab5e06af9ca654584
alebusi <allebusi@gmail.com>
2020-06-05 18:12:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -102,11 +102,13 @@ function cambioColore(div) {
 function cambioCampione() {
   if (accelero) {
       accelero=false;
+      mode=0;
       x=300;
       y=300;
   }
   else {    
       accelero=true;
+      mode=1;
   }
   ind_c+=1;
   if (ind_c > colori.length-1) ind_c=0;
