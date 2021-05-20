d912c7b666b3db17ba57f9878820f36ad7198531
alebusi <allebusi@gmail.com>
2021-05-20 10:40:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var ind_anim = 0;
 x = 300;
 y = 300;
 speed = 0;
-max_speed = 12;
+max_speed = 8;
 accelero=false;
 angle = 0;
 mod = 0;
@@ -25,7 +25,7 @@ var moveInterval = setInterval(function () {
 
 var guidaInterval = setInterval(function () {
     guida();
-}, 2000);
+}, 3000);
 
 function randomIntFromInterval(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
